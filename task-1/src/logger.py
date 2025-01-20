cd1b9467a60af555fb9dca95dfccf8dbe5cc2120
Alexander Kuksouski <akuksouski@gmail.com>
2025-01-20 10:34:24 +0100

    added tests, minor refactoring

--- task-1/src/logger.py
+++ task-1/src/logger.py
@@ -1,20 +1,23 @@
 import logging
 
+
 class Logger:
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = logging.getLogger('Task1Logger')
+            cls._instance = logging.getLogger("Task1Logger")
             cls._instance.setLevel(logging.INFO)
 
-            handler = logging.FileHandler('export/execution.log')
+            handler = logging.FileHandler("./export/execution.log")
             handler.setLevel(logging.INFO)
 
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
             handler.setFormatter(formatter)
             cls._instance.addHandler(handler)
         return cls._instance
 
-logger = Logger()
 
+logger = Logger()
